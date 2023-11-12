93de0001630aa7bbe1d4d0239b763793658a509c
Yura <yuraborisov19@gmail.com>
2023-11-12 12:58:50 +0200

    corrected slider

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,30 +1,22 @@
 (function main() {
-  const swipperGalleryFooter = new Swiper(".swiper-gallery-footer", {
-    initialSlide: 4,
-    slidesPerView: 1,
-
+  const thumbs = new Swiper(".swiper-gallery-footer", {
     allowTouchMove: false,
-
-    320: {
-      initialSlide: 1,
-      slidesPerView: 1,
-    },
-
-    600: {
-      initialSlide: 2,
-      slidesPerView: 1,
-    },
-
-    1700: {
-      initialSlide: 1,
-      slidesPerView: 1,
-    },
+    centeredSlides: true,
+    loopedSlides: 4, 
+    initialSlide: 1,
+    loop: true,
   });
 
-  new Swiper(".swiper", {
+  const slider = new Swiper(".gallery-swiper", {
+    slideToClickedSlide: false,
     grabCursor: true,
-    slideToClickedSlide: true,
-    initialSlide: 2,
+    slidesPerView: 4,
+    spaceBetween: 50,
+    initialSlide: 1,
+    loop: true,
+
+    loopedSlides: 4,
+    centeredSlides: true,
 
     keyboard: {
       enabled: true,
@@ -35,61 +27,46 @@
       prevEl: ".swiper-button-prev",
     },
 
-    thumbs: {
-      swiper: swipperGalleryFooter,
-    },
-
     breakpoints: {
       320: {
-        centeredSlides: true,
-        initialSlide: 1,
         slidesPerView: 1.3,
         spaceBetween: 10,
       },
 
       500: {
-        centeredSlides: true,
-        initialSlide: 1,
         slidesPerView: 1.5,
         spaceBetween: 10,
       },
 
       600: {
-        centeredSlides: true,
-        initialSlide: 2,
         slidesPerView: 1.7,
         spaceBetween: 30,
       },
 
       800: {
-        centeredSlides: true,
-        initialSlide: 2,
         slidesPerView: 2,
         spaceBetween: 40,
       },
 
       1000: {
-        centeredSlides: true,
-        initialSlide: 2,
         slidesPerView: 2.5,
         spaceBetween: 40,
       },
 
       1300: {
-        initialSlide: 2,
         slidesPerView: 3,
         spaceBetween: 50,
-        centeredSlides: true,
       },
 
       1700: {
-        initialSlide: 1,
         slidesPerView: 4,
         spaceBetween: 50,
-        centeredSlides: true,
       },
     },
   });
+
+  slider.controller.control = thumbs;
+  thumbs.controller.control = slider;
 
   const burgerIcon = document.querySelector("#burger-icon");
   const mobileMenu = document.querySelector("#mobile-menu");
